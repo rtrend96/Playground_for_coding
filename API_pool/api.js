de0897c39d3f43f16635ce46efc98282872f63d3
Rushikesh Nage <85242495+rushikesh-nage@users.noreply.github.com>
2022-02-17 19:22:50 -0600

    API-1 api call using normal XMLHttp req

--- API_pool/api.js
+++ API_pool/api.js
@@ -3,3 +3,10 @@
 function hello(){
     console.log("hellooo in another js");
 }
+
+let req = new XMLHttpRequest();
+req.open("GET","https://catfact.ninja/fact");
+req.send();
+req.onload = () =>{
+ console.log(req);
+}
