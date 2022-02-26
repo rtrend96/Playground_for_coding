c2379f9a8578687f3576256643ff628c08e8cca6
Rushikesh Nage <rtrend96@gmail.com>
2022-02-25 20:23:03 -0600

    update API

--- API_pool/api.js
+++ API_pool/api.js
@@ -3,10 +3,33 @@
 function hello(){
     console.log("hellooo in another js");
 }
-
+ // using normal http request
 let req = new XMLHttpRequest();
-req.open("GET","https://catfact.ninja/fact");
+req.open("GET","https://www.boredapi.com/api/activity");
 req.send();
 req.onload = () =>{
- console.log(req);
+ //console.log(req);
+ //console.log(JSON.parse(req.response))
 }
+
+//fetch
+
+fetch("https://www.boredapi.com/api/activity")
+.then(response =>{
+   // console.log(response);
+    return response.json();
+})
+
+//async await 
+async function helloapi(){
+    let response = await fetch("https://www.boredapi.com/api/activity");
+    let data = await response.json();
+    return data;
+}
+helloapi().then(data => console.log(data))
+
+//axios 
+
+axios.get("https://api.ipify.org?format=json").then(response =>{
+   console.log(response)  
+})
