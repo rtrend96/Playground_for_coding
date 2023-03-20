6486e379e20d3c3449c4068fea88eb08d4314e05
rtrend96 <85242495+rushikesh-nage@users.noreply.github.com>
2023-03-20 07:02:18 -0500

    Day 1 for Python

---
+++ Python/day1.py
@@ -0,0 +1,38 @@
+# print("Hello")
+# #For loop structure
+# sharks =["Rushi","Pallavi", "Manasi", "Kunal", "Ram", "Ramesh"]
+# for shark in sharks:
+#     print(shark)
+#------
+
+
+
+
+#integer value 
+# q1 = 34
+# q2= 45.55
+# q3 = q2 -q1
+# print(q3)
+
+# q4= 34>75
+# print(q4)
+#-----
+
+
+
+
+#working with String
+# print("HEllo " + "Bye") 
+# print ("Hello" + 34) # can't concat string and number
+# string replication
+# print("Rushi "*9)
+
+# print("1.   Rushi 2.     Make a dinner")
+#-----
+
+
+#string function
+# ss = "HELLOOOOO"
+# cc= "asdqwedqweqw"
+# print(ss.lower())
+# print(cc.upper())
